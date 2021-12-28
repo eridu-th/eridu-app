f60f56fcf1c6bf51d730f611fb3aed0f255a5dee
allenlin90 <ht016746@gmail.com>
2021-12-28 16:54:37 +0700

    add future lab shop id 276

--- server/routers/carry.js
+++ server/routers/carry.js
@@ -13,120 +13,134 @@ const carryHost = process.env.CARRY_HOST;
 // const carryTestPassword = process.env.CARRY_TEST_PASSWORD;
 
 router.post('/carry/login', checkHeaders, auth, async (req, res) => {
-    let resCode = 500;
-    let data = null;
+  let resCode = 500;
+  let data = null;
 
-    const { carryUsername, carryPassword } = carryUser(parseInt(req.user.carry_shop_id));
+  const { carryUsername, carryPassword } = carryUser(
+    parseInt(req.user.carry_shop_id),
+  );
 
-    try {
-        const response = await axios({
-            method: 'post',
-            header: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            url: `${carryHost}/api/auth`,
-            data: qs.stringify({
-                type: 'login',
-                username: carryUsername,
-                password: carryPassword,
-            }),
-        });
+  try {
+    const response = await axios({
+      method: 'post',
+      header: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      url: `${carryHost}/api/auth`,
+      data: qs.stringify({
+        type: 'login',
+        username: carryUsername,
+        password: carryPassword,
+      }),
+    });
 
-        resCode = response.data.status;
-        data = response.data;
-    } catch (err) {
-        data = err;
-    }
+    resCode = response.data.status;
+    data = response.data;
+  } catch (err) {
+    data = err;
+  }
 
-    res.status(resCode).send(data);
+  res.status(resCode).send(data);
 });
 
 router.post('/carry/order', checkHeaders, auth, async (req, res) => {
-    let result = {
-        resCode: 500,
-        message: 'something went wrong on the server...',
-        data: null,
-    };
+  let result = {
+    resCode: 500,
+    message: 'something went wrong on the server...',
+    data: null,
+  };
 
-    try {
-        if (req.body.token && req.body.data) {
-            const response = await callCarry(`${carryHost}/api/order`, req.body.token, req.body.data);
-            result.resCode = response.data.status;
-            result.message = response.data.message;
-            result.data = response.data.data;
-        } else {
-            result.resCode = 401;
-            result.message = 'missing token or invalid payload parameters';
-        }
-    } catch (err) {
-        result.data = err;
+  try {
+    if (req.body.token && req.body.data) {
+      const response = await callCarry(
+        `${carryHost}/api/order`,
+        req.body.token,
+        req.body.data,
+      );
+      result.resCode = response.data.status;
+      result.message = response.data.message;
+      result.data = response.data.data;
+    } else {
+      result.resCode = 401;
+      result.message = 'missing token or invalid payload parameters';
     }
+  } catch (err) {
+    result.data = err;
+  }
 
-    res.status(result.resCode).send(result);
+  res.status(result.resCode).send(result);
 });
 
 router.post('/carry/product', checkHeaders, auth, async (req, res) => {
-    let result = {
-        resCode: 500,
-        message: 'something went wrong on the server...',
-        data: null,
-    };
-    try {
-        if (req.body.token && req.body.data) {
-            const response = await callCarry(`${carryHost}/api/product`, req.body.token, req.body.data);
-            result.resCode = response.data.status;
-            result.message = response.data.message;
-            result.data = response.data.data;
-        } else {
-            result.resCode = 401;
-            result.message = 'missing token or invalid payload parameters';
-        }
-    } catch (err) {
-        result.data = err
+  let result = {
+    resCode: 500,
+    message: 'something went wrong on the server...',
+    data: null,
+  };
+  try {
+    if (req.body.token && req.body.data) {
+      const response = await callCarry(
+        `${carryHost}/api/product`,
+        req.body.token,
+        req.body.data,
+      );
+      result.resCode = response.data.status;
+      result.message = response.data.message;
+      result.data = response.data.data;
+    } else {
+      result.resCode = 401;
+      result.message = 'missing token or invalid payload parameters';
     }
+  } catch (err) {
+    result.data = err;
+  }
 
-    res.status(result.resCode).send(result);
+  res.status(result.resCode).send(result);
 });
 
 async function callCarry(url = '', token = '', data = null) {
-    try {
-        const response = await axios({
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': token,
-            },
-            url,
-            data: qs.stringify(data),
-        });
+  try {
+    const response = await axios({
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        Authorization: token,
+      },
+      url,
+      data: qs.stringify(data),
+    });
 
-        return response;
-    } catch (err) {
-        return err.response;
-    }
+    return response;
+  } catch (err) {
+    return err.response;
+  }
 }
 
 module.exports = router;
 
 function carryUser(id = '') {
-    let carryUsername = process.env.CARRY_TEST_ACCOUNT;
-    let carryPassword = process.env.CARRY_TEST_PASSWORD;
-    if (id) {
-        switch (id) {
-            case 249:
-                carryUsername = process.env.CARRY_ACCOUNT_HH;
-                carryPassword = process.env.CARRY_PASSWORD_HH;
-                break;
-            case 257:
-                carryUsername = process.env.CARRY_ACCOUNT_CLP;
-                carryPassword = process.env.CARRY_PASSWORD_CLP;
-                break;
-            default:
-                break;
-        }
+  let carryUsername = process.env.CARRY_TEST_ACCOUNT;
+  let carryPassword = process.env.CARRY_TEST_PASSWORD;
+  if (id) {
+    switch (id) {
+      case 249:
+        carryUsername = process.env.CARRY_ACCOUNT_HH;
+        carryPassword = process.env.CARRY_PASSWORD_HH;
+        break;
+      case 257:
+        carryUsername = process.env.CARRY_ACCOUNT_CLP;
+        carryPassword = process.env.CARRY_PASSWORD_CLP;
+        break;
+      case 276:
+        carryUsername = process.env.CARRY_ACCOUNT_FT;
+        carryPassword = process.env.CARRY_PASSWORD_FT;
+        break;
+      default:
+        break;
     }
-    return {
-        carryUsername,
-        carryPassword,
-    };
+  }
+  return {
+    carryUsername,
+    carryPassword,
+  };
 }
